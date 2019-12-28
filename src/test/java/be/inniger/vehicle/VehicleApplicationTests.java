59dbb9eeca0b7cf2f2661d1f633c93a454f1b9c9
Bram Inniger <bram.inniger@transferwise.com>
2019-12-28 22:47:57 +0200

    Initial commit, thanks spring initialiser!

---
+++ src/test/java/be/inniger/vehicle/VehicleApplicationTests.java
@@ -0,0 +1,13 @@
+package be.inniger.vehicle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
